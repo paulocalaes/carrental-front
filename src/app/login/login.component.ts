20156764bb7a7cf196530ec423528df252ac6f57
Paulo Calaes <paulocalaes@gmail.com>
2017-10-18 15:46:08 -0200

    Login Validators

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,6 +3,7 @@ import {Router} from '@angular/router';
 import { NgForm }   from '@angular/forms';
 import { Http, Headers } from '@angular/http';
 import 'rxjs/add/operator/toPromise';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Injectable()
 @Component({
@@ -12,18 +13,29 @@ import 'rxjs/add/operator/toPromise';
 })
 export class LoginComponent implements OnInit {
 	  title = 'Car Rental';
+
+    rForm: FormGroup;
+    post:any;
+    titleAlert:string = 'This field is required';
+
     email:string="";
     password:string="";
     //name:string="";
   	private headers = new Headers({'Content-Type': 'application/json'});
 
-  constructor(private router: Router, private _http: Http) { }
+  constructor(private router: Router, private _http: Http,private fb: FormBuilder) { 
+    this.rForm = fb.group({
+      'email' : [null, Validators.required],
+      'password' : [null, Validators.required]
+      
+    });
+  }
   public name = localStorage.getItem("name");
-  submit(form: NgForm):void{
+  submit(post):void{
 
       this._http.post(
           'http://api.triviasistemas.com.br/api/login', 
-          {email:this.email,password:this.password}
+          {email:post.email,password:post.password}
         ).subscribe(
           r=>{
             let data = r.json();
